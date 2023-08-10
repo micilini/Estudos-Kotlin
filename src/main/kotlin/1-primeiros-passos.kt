9bca5d914e3c619d872f855d0d1dcc0a84a1a7b2
micilini <sdanz@hotmail.com>
2023-08-10 16:23:08 -0300

    Conteúdo: Primeiros Passos com Kotlin

---
+++ src/main/kotlin/1-primeiros-passos.kt
@@ -0,0 +1,19 @@
+fun main() {//A função Main é o primeiro bloco de código que o compilador procura quando você roda o projeto. Ela também é conhecida como ponto de entrada.
+    //A função Main é única e deve existir somente uma por arquivo.
+
+    println("Olá, Mundo do Android!")//Código usado para imprimir uma linha, usando um texto do tipo STRING
+
+    print("Olá, Mundo do Android!")//Código usado para imprimir uma linha sem fazer a quebra de linha. Aqui estamos mostrando um texto do tipo STRING.
+
+    //Isso é um comentário e ele é ignorado pelo programa, só serve para nos situar..
+
+    /*
+    Isso aqui
+    é um comentário
+    de múltiplas linhas
+     */
+
+    //Códigos no Kotlin precisa de uma declaração do tipo TOPLEVEL, ou seja, não existem comandos soltos sem estarem algum bloco ou função.
+
+    //Para formatar a identação usamos o [CTRL] + [ALT] + [R]. Ou você pode ir no menu superior em Code > Reformat Code.
+}
