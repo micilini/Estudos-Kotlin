80137fc9aa74031e89f44a4afb50ce29f14081f9
micilini <sdanz@hotmail.com>
2023-08-13 14:06:19 -0300

    Conteúdo: NullSafety e Exceções

---
+++ src/main/kotlin/22-nullSafety.kt
@@ -0,0 +1,43 @@
+fun main() {
+    //Null Safety:
+
+    //var s: String = null//Não conseguimos atribuir nulo a variáveis não nulas
+
+    var s: String? = null//Com o ponto de interrogação, podemos atribuir nulo a variável (seja ela de qualquer tipo)
+
+    println(s?.length)//Quando usamos o ponto de interrogação, o compilador não gera uma exceção. Isso é o Kotlin fazendo o tratamento do nulo para nós
+    println(s!!.length)//"Não trate, eu tenho certeza que a variável não é nula, e caso for, eu assumo o problema"
+    println(s.length)//Se colocar de forma solta, o compilador não roda o projeto
+
+    //Quando usamos o ? depois da varíavel, dizemos ao Kotlin para tratar o Null caso aja algum problema
+    //Quando usamos o !! depois da variável, diz ao Kotlin que nós iremos assumir o erro
+
+    var texto = readlnOrNull()//Aqui estamos tratando caso venha nulo
+
+    if(texto == null || texto == ""){//Estamos verificando se o valor veio nullo
+        println("Voce não digitou nada no console")
+    }
+
+    //Variável Let
+
+    //Observe a seguinte estrutura abaixo que usamos para
+    //verificar se a string é nula, e caso não for, executa o bloco do if
+
+    val textor: String? = null
+
+    if(textor != null){
+        println(textor.lowercase())
+        println(textor.length)
+    }
+
+    //Observe como let funciona
+
+    textor?.let{
+        //O corpo do let so é executado caso o textor for diferente de nulo
+        //Aqui dentro é uma espécie de escopo de função
+        //nesse caso a variável passa a se chamar de it
+        println(it.lowercase())
+        println(it.length)
+    }//Podemos dizer que é basicamente um if/else disfarçado
+
+}
---
+++ src/main/kotlin/23-excecoes.kt
@@ -0,0 +1,60 @@
+fun main() {
+    //Excesões (Try Catch)
+
+    //Exemplo de código que vai dar erro ao ser executado
+    val s: String? = null
+    println(s!!.length)
+
+    //Forma de usar o Try Catch
+
+    try{//Tenta executar o comando sem erros, se der erros o catch é executado
+        println(s!!.length)//Isso vai dar erro e vai executar o bloco do catch
+    }catch (e: NullPointerException){//Variável temporaria que captura exceções do tipo NullPointer
+        println("Variável Nula")
+    }
+
+    println("Fim.")//O compilador ainda executará esta linha, isso é uma prova que mesmo após o erro, o sistema continua funcionando
+
+    //Podemos ter mais um tipo de catch declarado no Try, mas somente um único try
+    //Podemos ter varios tipos de exceções: https://kotlinlang.org/docs/exceptions.html
+
+    try{
+        val a = 10/0
+    }catch(e: NullPointerException){
+        println("Variável Nula")
+    }catch(e: ArithmeticException){
+        println("Impossível dividir por zero...")
+    }
+
+    //Existe uma palavra chamada 'Finally' que pode ser usado com try catch, ele é executado sempre (independente se der erro ou não)
+
+    try{
+        val a = 10/0
+    }catch(e: Exception){
+        println("Exceção Genérica que contém todos os tipos")
+    }catch(e: NullPointerException){
+        println("Variável Nula")
+    }catch(e: ArithmeticException){
+        println("Impossível dividir por zero...")
+    }finally {
+        println("Final dos blocos Try...Catch")
+    }
+
+    //Tenha em mente que na expressão acima, os erros de 'NullPointerException' e 'ArithmeticException' nunca irão ser executados, mesmo que o tipo de exceção seja esta.
+    //Isso acontece, pois temos acima desses dois, o erro de 'Exception' que é a forma mais generica, e assim, o Kotlin executa ele, ignorando os demais.
+    //O que poderíamos fazer é trocar a ordem:
+
+    try{
+        val a = 10/0
+    }catch(e: NullPointerException){
+        println("Variável Nula")
+    }catch(e: ArithmeticException) {
+        println("Impossível dividir por zero...")
+    }catch(e: Exception){
+        println("Exceção Genérica que contém todos os tipos")
+    }finally {
+        println("Final dos blocos Try...Catch")
+    }
+
+
+}
